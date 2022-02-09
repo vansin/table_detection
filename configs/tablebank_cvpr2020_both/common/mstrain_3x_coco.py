352df5b5a37f4974cbeda15cae83bd03e527c3cb
vansin <vansin@aliyun.com>
2022-02-10 00:14:19 +0800

    feat: datasets path

--- configs/tablebank_cvpr2020_both/common/mstrain_3x_coco.py
+++ configs/tablebank_cvpr2020_both/common/mstrain_3x_coco.py
@@ -1,7 +1,7 @@
 _base_ = '../_base_/default_runtime.py'
 # dataset settings
-dataset_type = 'CocoDataset'
-data_root = 'data/coco/'
+dataset_type = 'TableDataset'
+data_root = 'data/table/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 
@@ -46,20 +46,21 @@ data = dict(
         times=3,
         dataset=dict(
             type=dataset_type,
-            ann_file=data_root + 'annotations/instances_train2017.json',
-            img_prefix=data_root + 'train2017/',
+            ann_file=data_root + 'annotations/both_train.json',
+            img_prefix=data_root + 'images/',
             pipeline=train_pipeline)),
     val=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/instances_val2017.json',
-        img_prefix=data_root + 'val2017/',
+        ann_file=data_root + 'annotations/both_test.json',
+        img_prefix=data_root + 'images/',
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/instances_val2017.json',
-        img_prefix=data_root + 'val2017/',
+        ann_file=data_root + 'annotations/both_test.json',
+        img_prefix=data_root + 'images/',
         pipeline=test_pipeline))
-evaluation = dict(interval=1, metric='bbox')
+# evaluation = dict(interval=1, metric='bbox')
+evaluation = dict(metric='mAP', interval=200)
 
 # optimizer
 optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
